fc0bd109ead4bae4e992a8083af44d462eaeb246
mxudong <2941884109@qq.com>
2019-10-08 14:22:24 +0800

    添加测试代码

--- src/main/java/day02/Test.java
+++ src/main/java/day02/Test.java
@@ -1,5 +1,7 @@
 package day02;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -8,6 +10,29 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List list;
+        List<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+
+        System.out.println(list);
+
+        //排序：奇数在前 偶数在后 小数在前
+        list.sort(((Comparator<Integer>) (o1, o2) -> {
+            if (o1 % 2 == o2 % 2) {
+                return 0;
+            }
+            return o1 % 2 == 0 ? 1 : -1;
+        }).thenComparingInt(o -> o));
+
+        System.out.println(list);
     }
 }
