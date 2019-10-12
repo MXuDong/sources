aae763acf8cf9057bed97cc2e837f5581d579b25
mxudong <2941884109@qq.com>
2019-10-12 17:21:30 +0800

    SOLID Complete

---
+++ src/main/java/sources/collections/c_array_list/README.md
@@ -0,0 +1,5 @@
+# ArrayList
+type : class
+
+public class ArrayList<E> extends AbstractList<E> implements List<E>, RandomAccess, Cloneable, java.io.Serializable
+# 类文档
---
+++ src/main/java/sources/collections/c_array_list/Test.java
@@ -0,0 +1,13 @@
+package sources.collections.c_array_list;
+
+import java.util.ArrayList;
+
+/**
+ * @author Dong
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList arrayList;
+    }
+}
